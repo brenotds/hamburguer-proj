26125b3e8192b919209cc3642b7fb9b204c4624e
Breno <tardellibreno@gmail.com>
2024-08-20 20:50:03 +0100

    project able to be deployed

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function updateCartModal() {
             <p class="font-medium mt-2">£${item.price.toFixed(2)}</p>
         </div>
         <div>
-            <button>
+            <button class="remove-from-cart-btn" data-name="${item.name}">
                Remove
             </button>
         </div>    
@@ -84,4 +84,94 @@ function updateCartModal() {
   });
 
   cartTotal.textContent = total.toLocaleString("en", { style: "currency", currency: "GBP" });
+
+  cartCounter.innerHTML = cart.length;
+}
+
+cartItemsContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("remove-from-cart-btn")) {
+    const name = e.target.getAttribute("data-name");
+    removeItemCart(name);
+  }
+});
+
+function removeItemCart(name) {
+  const index = cart.findIndex((item) => item.name === name);
+
+  if (index !== -1) {
+    const item = cart[index];
+
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+      updateCartModal();
+      return;
+    }
+    cart.splice(index, 1);
+    updateCartModal();
+  }
+}
+
+addressInput.addEventListener("input", function (e) {
+  let inputValue = e.target.value;
+
+  if (inputValue !== "") {
+    addressInput.classList.remove("border-red-500");
+    addressWarn.classList.add("hidden");
+  }
+});
+
+checkoutBtn.addEventListener("click", function () {
+  const isOpen = openTime();
+  if (!isOpen) {
+    Toastify({
+      text: "Ops, restaurant is closed!",
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "#ef4444",
+      },
+    }).showToast();
+    return;
+  }
+
+  if (cart.length === 0) return;
+  if (addressInput.value === "") {
+    addressWarn.classList.remove("hidden");
+    addressInput.classList.add("border-red-500");
+    return;
+  }
+
+  const cartItems = cart
+    .map((item) => {
+      return `${item.name} qnt:(${item.quantity}) £ ${item.price} | `;
+    })
+    .join("");
+
+  const message = encodeURIComponent(cartItems);
+  const phone = "123456";
+
+  window.open(`https://wa.me/${phone}?text=${message} address: ${addressInput.value}`, "_blank");
+
+  cart = [];
+  updateCartModal();
+});
+
+function openTime() {
+  const data = new Date();
+  const hour = data.getHours();
+  return hour >= 18 && hour < 22;
+}
+
+const spanItem = document.getElementById("date-span");
+const isOpen = openTime();
+
+if (isOpen) {
+  spanItem.classList.remove("bg-red-500");
+  spanItem.classList.add("bg-green-600");
+} else {
+  spanItem.classList.remove("bg-green-600");
+  spanItem.classList.add("bg-red-500");
 }
